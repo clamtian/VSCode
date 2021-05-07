bba25dcf15566a01c053fb79c1f6c0fc50a44433
clamtian <tianshaowenwen@163.com>
2021-05-07 20:39:36 +0800

    lenovo push

--- LeetCode/1.两数之和.cpp
+++ LeetCode/1.两数之和.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem1.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=1 lang=cpp
  *
@@ -8,7 +16,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        int n = nums.size();
+        vector<int> res;
+        if(n < 2)
+            return res;
+        unordered_map<int, int> hmap;
+        for (int i = 0; i < n; ++i){
+            if (hmap.find(target - nums[i]) != hmap.end()){
+                res = { hmap[target - nums[i]] , i };
+                return res;
+            }
+            hmap[nums[i]] = i;
+        }
+        return res;
     }
 };
 // @lc code=end
