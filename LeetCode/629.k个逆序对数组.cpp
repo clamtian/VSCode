0c98c78d843b5bc1eedf5f52b9d717856da1dfc9
zhyuzh <luckytsw@163.com>
2021-05-21 13:49:13 +0800

    210521_desktop

--- LeetCode/629.k个逆序对数组.cpp
+++ LeetCode/629.k个逆序对数组.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem629.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=629 lang=cpp
  *
@@ -10,14 +18,15 @@ public:
     int kInversePairs(int n, int k) {
         if(k == 0) return 1;
         int MOD = 1e9 + 7;
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-        for(int i = 2; i <= n; ++i){
+        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, 0));
+        for(int i = 0; i <= n; ++i){
             dp[i][0] = 1;
         }
-        for(int j = 1; j <= k; ++j){
-            for(int i = 1; i <= n; ++i){
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-                if(dp[i][j] > MOD) dp[i][j] %= MOD;
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= k; ++j){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if(j >= i) dp[i][j] += MOD - dp[i - 1][j - i];
+                if(dp[i][j] >= MOD) dp[i][j] %= MOD;
             }
         }
         return dp[n][k];
