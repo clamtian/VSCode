2908c72d3df4f5690da1bebbe54983bf01551f83
zhyuzh <luckytsw@163.com>
2021-06-07 14:57:55 +0800

    20210607_zuo

---
+++ cppDemo/coinsWay.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int coins1(vector<int>& coins, int aim){
+        int n = coins.size();
+        if(aim <= 0) return aim == 0 ? 1 : 0;
+        vector<vector<int>> map(n + 1, vector<int>(aim + 1, -1));
+        return process(coins, 0, aim);
+    }
+    int process(vector<int>& coins, int pos, int aim, vector<vcctor<int>>& map){
+        if(map[pos][aim] != -1) return map[pos][aim];
+        int n = coins.size(), ans = 0;
+        if(pos == n){
+            ans = aim == 0 ? 1 : 0;
+        }else{
+            for(int i = 0; i * coins[pos] <= aim; ++i){
+                int value = map[pos + 1][aim - i * coins[pos]]
+                if(value != -1){
+                    ans += value;
+                }else{
+                    ans += process(coins, pos + 1, aim - i * coins[pos], map);
+                }
+            }
+        }
+        map[pos][aim] = ans;
+        return ans;
+    }
+};
