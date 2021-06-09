49057ca631fd490403129243725c733c92c71243
zhyuzh <luckytsw@163.com>
2021-06-09 14:54:01 +0800

    210609_desktop

--- cppDemo/0_LCDebug.cpp
+++ cppDemo/0_LCDebug.cpp
@@ -46,20 +46,33 @@ bool compare(const pair<string, int>& p1, const pair<string, int>& p2){
 }
 class Solution {
 public:
-    int stoneGameVIII(vector<int>& stones) {
-        int n = stones.size();
-        vector<int> sum(n + 1, 0);
-        for(int i = 0; i < n; ++i){
-            sum[i + 1] = sum[i] + stones[i];
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        int m = group.size();
+        int tot = accumulate(profit.begin(), profit.end(), 0);
+        int dp[n + 1][tot + 1][m + 1];
+        for(int i = 0; i <= n; ++i){
+            for(int j = 0; j <= tot; ++j){
+                for(int k = 0; k <= m; ++k){
+                    cout << i << endl;
+                        cout << j << endl;
+                        cout << k << endl;
+                    dp[i][j][k] = j == 0 ? 1 : 0;
+                }
+            }
         }
-        return solver(sum, 2);
-    }
-    int solver(vector<int>& sum, int pos){
-        int n = sum.size();
-        if(pos == n - 1) return sum[pos];
-        int ans = sum[pos];
-        for(int i = pos; i < n - 1; ++i){
-            ans = max(ans, sum[i] - solver(sum, i + 1));
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= tot; ++j){
+                for(int k = 1; k <= m; ++k){
+                    dp[i][j][k] = dp[i][j][k - 1];
+                    if(i >= group[k - 1] && j >= profit[k - 1]){
+                        dp[i][j][k] += dp[i - group[k - 1]][j - profit[k - 1]][k - 1];
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int j = minProfit; j <= tot; ++j){
+            ans += dp[n][j][m];
         }
         return ans;
     }
@@ -71,7 +84,8 @@ int main() {
     string str2 = "aba";
     cout << max(str1, str2) << endl;
     vector<vector<int>> a = { {12,4}, {8,1}, {6,3}};
-    vector<int> b = { 7,-6,5,10,5,-2, -6 };
+    vector<int> b = { 82,7,18,34,1,3,83,56,50,34,39,38,76,92,71,2,6,74,1,82,22,73,88,98,6,71,6,26,100,75,57,88,43,16,22,89,7,9,78,97,22,87,34,81,74,56,49,94,87,71,59,6,20,66,64,37,2,42,30,87,73,16,39,87,28,9,95,78,43,59,87,78,2,93,7,22,21,59,68,67,65,63,78,20,82,35,86 };
+    vector<int> b_ = { 45,57,38,64,52,92,31,57,31,52,3,12,93,8,11,60,55,92,42,27,40,10,77,53,8,34,87,39,8,35,28,70,32,97,88,54,82,54,54,10,78,23,82,52,10,49,8,36,9,52,81,26,5,2,30,39,89,62,39,100,67,33,86,22,49,15,94,59,47,41,45,17,99,87,77,48,22,77,82,85,97,66,3,38,49,60,66 };
 
     vector<vector<char>> ch_vec = { {'1', '1', '1'},
                                {'0', '1', '0'},
@@ -105,7 +119,7 @@ int main() {
     cout << atoi(st.c_str()) << endl;
     //cout << so.func(4, a) << endl;
     double ss = 2.01;
-    int x = so.stoneGameVIII(b);
+    int x = so.profitableSchemes(95,53,b,b_);
 
     return 0;
 }
