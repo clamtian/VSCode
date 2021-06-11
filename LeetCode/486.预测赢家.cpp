0b05ed8483c76bd7fa4c1582003ae1ea579d173a
zhyuzh <luckytsw@163.com>
2021-06-11 20:00:59 +0800

    210611_desktop

--- LeetCode/486.预测赢家.cpp
+++ LeetCode/486.预测赢家.cpp
@@ -18,17 +18,17 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        if(n < 3) return true; 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        if(n == 1) return true;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
         for(int i = 0; i < n; ++i){
-            dp[i][i] = nums[i];
+            dp[i][i + 1] = nums[i];
         }
-        for(int j = 1; j < n; ++j){
-            for(int i = j - 1; i >= 0; --i){
-                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+        for(int j = 1; j <= n; ++j){
+            for(int i = j - 2; i >= 0; --i){
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j - 1] - dp[i][j - 1]);
             }
         }
-        return dp[0][n - 1] >= 0;
+        return dp[0][n] >= 0;
     }
 };
 // @lc code=end
--- cppDemo/13_coinsWay.cpp
+++ cppDemo/13_coinsWay.cpp
@@ -1,3 +1,21 @@
+#include <string.h>
+#include <vector>
+#include <string>
+#include <list>
+#include <map> 
+#include <vector> 
+#include <algorithm>
+#include <set>
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <queue>
+#include <sstream> 
+
+using namespace std;
+
 class Solution {
 public:
     int coins1(vector<int>& coins, int aim){
@@ -13,7 +31,7 @@ public:
             ans = aim == 0 ? 1 : 0;
         }else{
             for(int i = 0; i * coins[pos] <= aim; ++i){
-                int value = map[pos + 1][aim - i * coins[pos]]
+                int value = map[pos + 1][aim - i * coins[pos]];
                 if(value != -1){
                     ans += value;
                 }else{
