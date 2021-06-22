976aa729d27aeef801e690a0cc9fe14353b0f0f3
zhyuzh <luckytsw@163.com>
2021-06-22 09:59:35 +0800

    210621_md

--- cppDemo/0_cppTest_Win.cpp
+++ cppDemo/0_cppTest_Win.cpp
@@ -9,64 +9,40 @@
 #include <algorithm>
 #include <set>
 #include <iostream>
+#include <fstream>
 #include <stack>
 #include <unordered_map>
 #include <unordered_set>
 #include <numeric>
 #include <queue>
 #include <sstream> 
+
 using namespace std;
 
-class Base
-{
-public:
-    int pub_ele = 1;
-    virtual void func(){
-        cout << "Base func()! " << endl;
-    };
-protected: 
-    int prot_ele = 2;
-private:
-    int priv_ele = 3;
-};
-
-class Derive : public Base
-{
-public:
-    int ele = 0;
-    void func(){
-        cout << "Derive func()! " << endl;
-    }
-};
-
-
-vector<vector<int>> deleteRow(vector<vector<int>>& vecs, set<int>& rows){
-    vector<vector<int>> res;
-    int n = vecs.size();
-    for(int i = 0; i < n; ++i){
-        if(rows.find(i) == rows.end()){
-            res.push_back(vecs[i]);
-        }
-    }
-    return res;
-}
-
-int f(int m, int n){
-    if(m == 0){
-        return n + 1;
-    }else if(n == 0){
-        return f(m - 1, 1);
-    }else{
-        return f(m - 1, f(m, n - 1));
-    }
-}
 int main()
 {
-    vector<int> a = {3, 4, 5, 2 };
-    vector<int> b = {3, 4, 5, 1, 2 };
-    double c = inner_product(a.begin(), a.end(), b.begin(), 0.);
-    auto it = a.begin();
-    generate(a.begin(), a.end(), [&it](){ return (*it) * (*(it++)); });
+    string data_file = "./inputTest.txt";
+    ifstream infile(data_file.c_str());
+	if (!infile) {
+		cout << "open nas file failure !" << endl;
+		exit(0);
+	}
 
+	string line;
+
+	//get TrianglesNum
+	string str;
+	getline(infile, line);
+	stringstream sin(line);
+    int num = 0;
+    while(sin){
+        sin >> str;
+        if(str == "ss_prop1"){
+            sin >> num;
+            cout << num << endl;
+            sin >> num; 
+            sin >> num; 
+        }
+    }
     return 1;
 }
