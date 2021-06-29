5164cab523b218c23b7c2c01559dbce11b1c1a61
zhyuzh <luckytsw@163.com>
2021-06-29 18:31:52 +0800

    210629_lc

--- LeetCode/752.打开转盘锁.cpp
+++ LeetCode/752.打开转盘锁.cpp
@@ -17,102 +17,54 @@ using namespace std;
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
+        if(target == "0000") return 0;
+        queue<pair<string, int>> q;
+        q.push({ "0000", 0 });
         unordered_set<string> uset(deadends.begin(), deadends.end());
-        vector<int> dp1(10000, -2), dp2(10000, -2);
-        dp1[0] = dp2[0] = 0;
-        int cmp1 = dfs1(uset, target, dp1);
-        cout << cmp1 << endl;
-        int cmp2 = dfs2(uset, target, dp2, dp1);
-        cout << cmp2 << endl;
-        int ans = min(cmp1, cmp2);
-        return ans == -1 ? max(cmp1, cmp2) : ans;
-    }
-    int dfs2(unordered_set<string>& uset, string t, vector<int>& dp, vector<int>& dp1){
-        if(dp[stoi(t)] == -2){
-            if(uset.find(t) != uset.end()){
-                dp[stoi(t)] = -1;
-            }else{
-                string s;
-                int cmp, tmp = INT_MAX;
-
-                s = t;
-                if(t[0] != '0'){
-                    s[0] = t[0] - 1;
-                    cmp = dfs2(uset, s, dp, dp1);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
+        while(!q.empty()){
+            auto [loc, step] = q.front();
+            q.pop();
+            vector<string> cand = getCand(loc);
+            for(string s : cand){
+                if(s == target) return step + 1;
+                if(uset.find(s) == uset.end()){
+                    q.push({ s, step + 1 });
+                    uset.insert(s);
                 }
-                
-                s = t;
-                if(t[1] != '0'){
-                    s[1] = t[1] - 1;
-                    cmp = dfs2(uset, s, dp, dp1);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-
-                s = t;
-                if(t[2] != '0'){
-                    s[2] = t[2] - 1;
-                    cmp = dfs2(uset, s, dp, dp1);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-
-                s = t;
-                if(t[3] != '0'){
-                    s[3] = t[3] - 1;
-                    cmp = dfs2(uset, s, dp, dp1);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-                if(tmp == INT_MAX) dp[stoi(t)] = -1;
-                else dp[stoi(t)] = tmp;
             }
-        } 
-        if(dp1[stoi(t)] != -1) 
-            dp[stoi(t)] = dp[stoi(t)] == -1 ? dp1[stoi(t)] : min(dp[stoi(t)], dp1[stoi(t)]);
-        return dp[stoi(t)];
+        }
+        return -1;
     }
-    int dfs1(unordered_set<string>& uset, string t, vector<int>& dp){
-        if(dp[stoi(t)] == -2){
-            if(uset.find(t) != uset.end()){
-                dp[stoi(t)] = -1;
-            }else{
-                string s;
-                int cmp, tmp = INT_MAX;
+    vector<string> getCand(string s){
+        vector<string> res;
+        string t = s;
+        t[0] = t[0] == '9' ? '0' : t[0] + '1';
+        res.push_back(t);
+        t = s;
+        t[0] = t[0] == '0' ? '9' : t[0] - '1';
+        res.push_back(t);
+        t = s;
+        t[1] = t[1] == '9' ? '0' : t[1] + '1';
+        res.push_back(t);
+        t = s;
+        t[1] = t[1] == '0' ? '9' : t[1] - '1';
+        res.push_back(t);
+        t = s;
+        t[2] = t[2] == '9' ? '0' : t[2] + '1';
+        res.push_back(t);
+        t = s;
+        t[2] = t[2] == '0' ? '9' : t[2] - '1';
+        res.push_back(t);
+        t = s;
+        t[3] = t[3] == '9' ? '0' : t[3] + '1';
+        res.push_back(t);
+        t = s;
+        t[3] = t[3] == '0' ? '9' : t[3] - '1';
+        res.push_back(t);
 
-                s = t;
-                if(t[0] != '9'){
-                    s[0] = t[0] + 1;
-                    cmp = dfs1(uset, s, dp);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-
-                s = t;
-                if(t[1] != '9'){
-                    s[1] = t[1] + 1;
-                    cmp = dfs1(uset, s, dp);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-
-                s = t;
-                if(t[2] != '9'){
-                    s[2] = t[2] + 1;
-                    cmp = dfs1(uset, s, dp);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-
-                s = t;
-                if(t[3] != '9'){
-                    s[3] = t[3] + 1;
-                    cmp = dfs1(uset, s, dp);
-                    if(cmp != -1) tmp = min(tmp, cmp + 1);
-                }
-
-                if(tmp == INT_MAX) dp[stoi(t)] = -1;
-                else dp[stoi(t)] = tmp;
-            }
-            
-        } 
-        return dp[stoi(t)];
+        return res;
     }
+    
 };
 // @lc code=end
 
