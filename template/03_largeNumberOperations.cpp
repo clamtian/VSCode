0808582ecf4d44f4a7f3f305b9613a50808ac448
zhyuzh <luckytsw@163.com>
2021-07-12 15:08:04 +0800

    210712_div

--- template/03_largeNumberOperations.cpp
+++ template/03_largeNumberOperations.cpp
@@ -63,6 +63,20 @@ vector<int> mul(vector<int> A, int b){
     return C;
 }
 
+// C = A * b A >= 0    0 < b <= 10000
+vector<int> div(vector<int>& A, int b, int& r){
+    vector<int> C;
+    r = 0;
+    for(int i = A.size() - 1; i >= 0; --i){
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r %= b;
+    }
+    reverse(C.begin(), C.end());
+    while(C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+
 
 vector<int> generateArndomArray(int maxSize, int maxValue){
     srand((unsigned)time(NULL));
