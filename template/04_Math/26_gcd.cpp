a71f2e4b401e43fd67c50d49dd86e35986cfbdf3
zhyuzh <luckytsw@163.com>
2021-08-30 22:00:47 +0800

    210830_2

---
+++ template/04_Math/26_gcd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 欧几里得算法求最大公约数
+
+
+int gcd(int a, int b){
+    return b ? gcd(b, a % b) : a;
+}
+
---
+++ template/04_Math/27_euler.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 求欧拉函数
+
+int phi(int x){
+    if(x < 2) return x;
+    int res = x;
+    for(int i = 2; i <= x / i; ++i){
+        if(x % i == 0){
+            res = res / i * (i - 1);
+            while(x % i == 0) x /= i;
+        }
+    }
+    if(x > 1) res = res / x * (x - 1);
+    return res;
+}
