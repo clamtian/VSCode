10f957ecb42a7fe6f8151a9f32feb758ee6bfce0
clamtian <tianshaowenwen@163.com>
2021-09-24 03:36:18 +0800

    210923

---
+++ LeetCode/430.扁平化多级双向链表.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=430 lang=cpp
+ *
+ * [430] 扁平化多级双向链表
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        
+    }
+};
+// @lc code=end
+
