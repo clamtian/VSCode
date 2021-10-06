9c0a8b79084dffd3216bdc49708d72bda3d2511f
clamtian <tianshaowenwen@163.com>
2021-10-06 21:28:00 +0200

    211007

--- cppDemo/00_cppTest_Win.cpp
+++ cppDemo/00_cppTest_Win.cpp
@@ -8,10 +8,24 @@
 
 using namespace std;
 
+struct myStruct{
+    uint16_t a;
+    myStruct *next;
+};
+
+
+
 int main(){
-    int x = -1, a = 15;
-    bitset<32> c(((a << 28) & x) >> 28);
-    cout << c << endl;
+    myStruct s[3];
+    s[0].a = 1, s[0].next = &s[1], s[1].a = 9, s[1].next = NULL;
+    myStruct *p1, *p2;
+    p1 = &s[0], p2 = &s[2];
+    cout << sizeof(s[0]) << endl;
+    cout << sizeof(p1) << endl;
+    cout << p1 << endl;
+    cout << p2 << endl;
+    cout << p2 - p1 << endl;
+    cout << ((p2 - p1) << 12) << endl;
 
 }
 
