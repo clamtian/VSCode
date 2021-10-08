25827ff9a18772dca27a5e09c253e05a19fb3cd8
clamtian <tianshaowenwen@163.com>
2021-10-08 20:57:07 +0200

    211008

--- template/02_Data Structure/12_strHash.cpp
+++ template/02_Data Structure/12_strHash.cpp
@@ -15,7 +15,7 @@ int P = 13331;
 
 ULL h[N], p[N]; // h[k]存储字符串前k个字母的哈希值, p[k]存储 P^k mod 2^64
 
-// 计算子串 str[l ~ r] 的哈希值
+// 计算子串 str[l + 1 ~ r + 1] 的哈希值 
 ULL get(int l, int r){
     return h[r] - h[l - 1] * p[r - l + 1];
 }
