693f7cd193593f34a3e51d15415ebbe2a549de77
clamtian <tianshaowenwen@163.com>
2021-10-23 23:46:52 +0200

    211023

--- cppDemo/00_cppTest_Win.cpp
+++ cppDemo/00_cppTest_Win.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
-#include <queue>
-#include <climits>
-#include <bitset>
 #include <algorithm>
+#include <unordered_map>
+
+typedef long long LL;
 
 using namespace std;
 
-struct myStruct{
-    uint16_t a;
-    myStruct *next;
-};
-
-
+int MOD = 1e9+7;
 
 int main(){
-    myStruct s[3];
-    s[0].a = 1, s[0].next = &s[1], s[1].a = 9, s[1].next = NULL;
-    myStruct *p1, *p2;
-    p1 = &s[0], p2 = &s[2];
-    cout << sizeof(s[0]) << endl;
-    cout << sizeof(p1) << endl;
-    cout << p1 << endl;
-    cout << p2 << endl;
-    cout << p2 - p1 << endl;
-    cout << ((p2 - p1) << 12) << endl;
-
+    int n, m; 
+    cin >> n >> m;
+    vector<vector<LL>> dp1(n + 1, vector<LL>(m, 0)), dp2(n + 1, vector<LL>(m, 0));
+    for(int i = 1; i <= n; ++i) dp1[i][0] = n - i + 1 + dp1[i - 1][0];
+    dp2[n][0] = n;
+    for(int i = n - 1; i > 0; --i) dp2[i][0] = i + dp2[i + 1][0];
+    for(int j = 1; j < m; ++j){
+        for(int i = 1; i <= n; ++i){
+            dp1[i][j] = dp1[i - 1][j] + dp1[i][j - 1] * (n - i + 1) % MOD;
+        }
+    }
+    for(int j = 1; j < m; ++j){
+        dp2[n][j] = dp2[n][j - 1];
+        for(int i = n - 1; i > 0; --i){
+            dp2[i][j] = dp2[i + 1][j] + dp2[i][j - 1] * i % MOD;
+        }
+    }
+    LL ans = 0;
+    for(int i = 1; i <= n; ++i){
+        ans += dp1[i][m - 1] * dp2[i][m - 1];
+        ans %= MOD;
+    }
+    cout << ans << endl;
+    return 0;
+    
 }
-
