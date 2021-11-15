71d1a707f41de5825e73741e78a778f38f36a997
clamtian <tianshaowenwen@163.com>
2021-11-15 14:17:32 +0100

    211115

--- cppDemo/00_cppTest_Win.cpp
+++ cppDemo/00_cppTest_Win.cpp
@@ -10,29 +10,8 @@ using namespace std;
 int MOD = 1e9+7;
 
 int main(){
-    int n, m; 
-    cin >> n >> m;
-    vector<vector<LL>> dp1(n + 1, vector<LL>(m, 0)), dp2(n + 1, vector<LL>(m, 0));
-    for(int i = 1; i <= n; ++i) dp1[i][0] = n - i + 1 + dp1[i - 1][0];
-    dp2[n][0] = n;
-    for(int i = n - 1; i > 0; --i) dp2[i][0] = i + dp2[i + 1][0];
-    for(int j = 1; j < m; ++j){
-        for(int i = 1; i <= n; ++i){
-            dp1[i][j] = dp1[i - 1][j] + dp1[i][j - 1] * (n - i + 1) % MOD;
-        }
-    }
-    for(int j = 1; j < m; ++j){
-        dp2[n][j] = dp2[n][j - 1];
-        for(int i = n - 1; i > 0; --i){
-            dp2[i][j] = dp2[i + 1][j] + dp2[i][j - 1] * i % MOD;
-        }
-    }
-    LL ans = 0;
-    for(int i = 1; i <= n; ++i){
-        ans += dp1[i][m - 1] * dp2[i][m - 1];
-        ans %= MOD;
-    }
-    cout << ans << endl;
+    vector<int> v = { 3, 6, 12, 15, 16, 19 };
+    auto a = lower_bound(v.begin(), v.end(), 17);
     return 0;
     
 }
