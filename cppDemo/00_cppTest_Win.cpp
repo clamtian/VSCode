9e1ce0a9db7ad70b5ff4a50120e190caa48c2287
clamtian <tianshaowenwen@163.com>
2021-12-11 20:42:47 +0100

    211211

--- cppDemo/00_cppTest_Win.cpp
+++ cppDemo/00_cppTest_Win.cpp
@@ -1,17 +1,37 @@
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 #include <unordered_map>
+#include <algorithm>
 
-typedef long long LL;
+typedef long int LL;
 
 using namespace std;
 
-int MOD = 1e9+7;
-
 int main(){
-    vector<int> v = { 3, 6, 12, 15, 16, 19 };
-    auto a = lower_bound(v.begin(), v.end(), 17);
+    int n;
+    cin >> n;
+    vector<vector<LL>> vec(2 * n);
+    int pos = 0;
+    for(int i = 0; i < n; ++i){
+        LL a, b;
+        scanf("%ld%ld", &a, &b);
+        vec[pos++] = { a, 0 };
+        vec[pos++] = { b, 1 };
+    }
+    sort(vec.begin(), vec.end());
+    vector<LL> res(n + 1, 0);
+    LL cnt = 0;
+    vector<LL> v = { 0 ,0 };
+    for(int i = 0; i < n * 2; ++i){
+        auto c = vec[i];
+        if(c[1] == 0) res[cnt] += c[0] - v[0] - 1, res[cnt + 1]++, ++cnt;
+        else res[cnt] += c[0] - v[0], --cnt;
+        v = c;
+    }
+    for(int i = 1; i <= n; ++i){
+        printf("%ld ", res[i]);
+    }
     return 0;
-    
 }
