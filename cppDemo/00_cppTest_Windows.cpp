91cdd5fd3526b381ccca45a6ec0dd34c0882739d
clamtian <tianshaowenwen@163.com>
2022-07-17 23:51:12 +0200

    220717

---
+++ cppDemo/00_cppTest_Windows.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+
+typedef long int LL;
+
+using namespace std;
+
+int main() {
+    cout << "hello" << endl;
+    return 0;
+}
