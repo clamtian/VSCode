75248e123e18cd39656241f62c3f2fc59bc0a8a8
clamtian <tianshaowenwen@163.com>
2022-07-26 00:14:56 +0200

    220726

--- cppDemo/00_cppTest_Windows.cpp
+++ cppDemo/00_cppTest_Windows.cpp
@@ -8,7 +8,16 @@ typedef long int LL;
 
 using namespace std;
 
+int sum(int& a, int b) { 
+    int p = a + b;
+    cout << &a << endl;
+    return a; 
+}
 int main() {
     cout << "hello" << endl;
+    int s = 0, d = 9;
+
+    cout << sum(s, d) << endl;
+    cout << sum(s, d) << endl;
     return 0;
 }
